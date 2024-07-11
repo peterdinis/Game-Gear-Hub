2a14e0669106976ec8b65d2dd44735623fa68d42
Peter Dinis <peterdinis0@pobox.sk>
2024-07-11 19:23:36 +0200

    FOO

--- app/_components/shared/ThemeButton.tsx
+++ app/_components/shared/ThemeButton.tsx
@@ -23,16 +23,27 @@ const ThemeButton: FC = () => {
     return (
         <div
             onClick={toggleTheme}
-            className="relative flex h-8 w-16 cursor-pointer items-center rounded-full bg-teal-500 p-1 dark:bg-gray-700"
+            className={`relative flex h-8 w-16 cursor-pointer items-center rounded-full p-1 ${
+                resolvedTheme === 'dark' ? 'bg-gray-700' : 'bg-teal-500'
+            }`}
         >
-            <Moon className="h-4 w-4 fill-white" />
             <div
                 id="toggleBtnTheme"
                 className={`absolute h-6 w-6 rounded-full bg-white shadow-customShadow-md transition-transform ${
                     resolvedTheme === 'dark' ? 'translate-x-8' : 'translate-x-0'
                 }`}
             ></div>
-            <Sun className="ml-auto h-4 w-4 fill-white" />
+            <div
+                className={`absolute flex items-center justify-center transition-transform ${
+                    resolvedTheme === 'dark' ? 'translate-x-8' : 'translate-x-0'
+                }`}
+            >
+                {resolvedTheme === 'dark' ? (
+                    <Sun className="h-4 w-4 fill-gray-700" />
+                ) : (
+                    <Moon className="h-4 w-4 fill-teal-500" />
+                )}
+            </div>
         </div>
     );
 };
