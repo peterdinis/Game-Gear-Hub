d381966e5bf2c8855fe564c6923261f5ec510787
Peter Dinis <peterdinis0@pobox.sk>
2024-07-11 20:21:18 +0200

    FOO

--- app/_components/shared/Header.tsx
+++ app/_components/shared/Header.tsx
@@ -6,7 +6,7 @@ interface IHeaderProps {
 
 const Header: FC<IHeaderProps> = ({ text }: IHeaderProps) => {
     return (
-        <h1 className='mt-10 scroll-m-20 text-center text-4xl font-extrabold tracking-tight lg:text-5xl'>
+        <h1 className="mt-24 text-center text-4xl font-extrabold tracking-tight lg:mt-10 lg:text-5xl">
             {text}
         </h1>
     );
