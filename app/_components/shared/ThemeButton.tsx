9ba3eb7b53ec3eff71be3247ad16f3bd9207fef7
Peter Dinis <peterdinis0@pobox.sk>
2024-07-11 19:16:06 +0200

    feat: Fixing theme button

--- app/_components/shared/ThemeButton.tsx
+++ app/_components/shared/ThemeButton.tsx
@@ -2,16 +2,14 @@
 
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
-import { type FC, useEffect, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 const ThemeButton: FC = () => {
     const [mounted, setMounted] = useState(false);
-    const [isActive, setIsActive] = useState(false);
     const { resolvedTheme, setTheme } = useTheme();
 
     const toggleTheme = () => {
         setTheme(resolvedTheme === 'light' ? 'dark' : 'light');
-        setIsActive(!isActive);
     };
 
     useEffect(() => {
@@ -25,18 +23,16 @@ const ThemeButton: FC = () => {
     return (
         <div
             onClick={toggleTheme}
-            className={`relative flex h-8 w-16 cursor-pointer items-center rounded-full bg-teal-500 p-1 dark:bg-gray-700`}
+            className="relative flex h-8 w-16 cursor-pointer items-center rounded-full bg-teal-500 p-1 dark:bg-gray-700"
         >
-            <Moon className='h-[17px] w-[17px] fill-white'></Moon>
+            <Moon className="h-4 w-4 fill-white" />
             <div
-                id='toggleBtnTheme'
-                className={`shadow-customShadow-md absolute h-6 w-6 rounded-full bg-white ${
-                    isActive
-                        ? 'translate-x-0 transition-transform'
-                        : 'translate-x-8 transition-transform'
+                id="toggleBtnTheme"
+                className={`absolute h-6 w-6 rounded-full bg-white shadow-customShadow-md transition-transform ${
+                    resolvedTheme === 'dark' ? 'translate-x-8' : 'translate-x-0'
                 }`}
             ></div>
-            <Sun className='ml-auto h-[17px] w-[17px] fill-white'></Sun>
+            <Sun className="ml-auto h-4 w-4 fill-white" />
         </div>
     );
 };
